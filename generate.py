aa81064d89376aee73e8067d45578b90c832520f
dBob <bobrovnykov@yahoo.com>
2024-12-27 14:52:18 +0700

    Assign comp v1

--- generate.py
+++ generate.py
@@ -170,7 +170,9 @@ class CrosswordCreator():
         Return True if `assignment` is complete (i.e., assigns a value to each
         crossword variable); return False otherwise.
         """
-        raise NotImplementedError
+        if len(assignment.keys()) == len(self.crossword.variables):
+            return True
+        return False
 
     def consistent(self, assignment):
         """
